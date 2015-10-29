d8e2e250f542061b2225d8b7179e824fae353d80
isweta <mailbox.sweta@gmail.com>
2015-10-29 00:57:15 -0700

    properties

---
+++ VzWeb/src/com/verizon/ConfigReader.java
@@ -0,0 +1,39 @@
+package com.verizon;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigReader {
+	
+	public  String returnVideoURL(String elementID){
+		Properties props = new Properties();
+	    InputStream is = null;
+	 
+	    // First try loading from the current directory
+	    try {
+	        File f = new File("videoResources.properties");
+	        is = new FileInputStream( f );
+	    }
+	    catch ( Exception e ) { is = null; }
+	 
+	    try {
+	        if ( is == null ) {
+	            // Try loading from classpath
+	            is = getClass().getResourceAsStream("videoResources.properties");
+	        }
+	 
+	        // Try loading properties from the file (if found)
+	        props.load( is );
+	    }
+	    catch ( Exception e ) { }
+	 
+	    String videoURL = props.getProperty("elementID", "NoResource");
+	    return videoURL;
+	}
+
+		
+	}
+
+
